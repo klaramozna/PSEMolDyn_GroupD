4b1e3b78a07ebdf31997f366491a8270305bee28
ge95dop <ge95dop@mytum.de>
2023-11-02 23:39:42 +0100

    Documenting VectorDouble

--- src/VectorDouble.h
+++ src/VectorDouble.h
@@ -9,32 +9,92 @@
 #include <vector>
 #include <array>
 
+/**
+ * @file Particle.h
+ *
+ * Provides vector operations using simple operators.
+ */
+
 class VectorDouble {
 private:
+    /**
+     * Stores values of the vector
+     */
     std::vector<double> values;
 public:
     VectorDouble() = default;
 
+    /**
+     * Creates a vector of the given size
+     * @param size is the size of the created vector
+     */
     explicit VectorDouble(std::vector<double>::size_type size);
 
+    /**
+     * Creates a new vector, using the values of the given vector
+     * @param vector is the vector, that values are copied from
+     */
     explicit VectorDouble(const std::vector<double> &vector);
 
+    /**
+     * Creates a vector of size 3, using the values of the given array
+     * @param array contains the values, given to the created vector
+     */
     explicit VectorDouble(const std::array<double, 3>& array);
 
+    /**
+     *  Adds other to this and returns the result
+     * @param other is another vector that has to be of the same size as the object this function is being invoked on
+     * @return The resulting vector of the addition
+     */
     VectorDouble operator+=(const VectorDouble &other);
 
+    /**
+     *  Subtracts other from this and returns the result
+     * @param other is another vector that has to be of the same size as the object this function is being invoked on
+     * @return The resulting vector of the subtraction
+     */
     VectorDouble operator-=(const VectorDouble &other);
 
+    /**
+     * Returns the L2 norm (euclidean norm) of the vector
+     * @return L2 norm of the vector
+     */
     double getL2Norm() const;
 
+    /**
+     * Prints the vector on stdout
+     */
     void print() const;
 
+    /**
+     * Converts the vector to ann array (only available for vectors of size 3)
+     * @return
+     */
     std::array<double, 3> convertToArray() const;
 
+    /**
+     * Adds two vectors and returns result
+     * @param vec1 left vector
+     * @param vec2 right vector
+     * @return resulting vector of the addition
+     */
     friend VectorDouble operator+(const VectorDouble &vec1, const VectorDouble &vec2);
 
+    /**
+     * Subtracts vec2 from vec1 and returns result
+     * @param vec1 left vector
+     * @param vec2 right vector
+     * @return resulting vector of the subtraction
+     */
     friend VectorDouble operator-(const VectorDouble &vec1, const VectorDouble &vec2);
 
+    /**
+     * Multiplies the vector with a scalar
+     * @param scalar is the value the vector is multiplied with
+     * @param vec is the vector that the scalar is multiplied with
+     * @return resulting vector
+     */
     friend VectorDouble operator*(double scalar, const VectorDouble &vec);
 };
 
