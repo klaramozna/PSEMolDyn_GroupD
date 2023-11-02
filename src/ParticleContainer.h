c22723576eca32f1d560fd28364997cd691e9030
ge95dop <ge95dop@mytum.de>
2023-11-03 00:27:14 +0100

    Documenting ParticleContainer, adding @brief to the documentation of VectorDouble

--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -8,35 +8,135 @@
 #include <vector>
 #include "Particle.h"
 
+/**
+ * @file ParticleContainer.h
+ *
+ * @brief Hides structure / implementation of particles.
+ */
+
 
 class ParticleContainer {
 private:
+    /**
+     * @brief Stores the particles.
+     */
     std::vector<Particle> particles;
 public:
     class PairIterator;
-    using iterator_type = std::vector<Particle>::iterator;
-    explicit ParticleContainer(std::vector<Particle> particles);
-    ParticleContainer() = default;
-    iterator_type begin();
-    iterator_type end();
-    void addParticle(Particle particle);
-    void addParticles(std::vector<Particle> toAdd);
-    PairIterator beginPair();
-    PairIterator endPair();
-    std::vector<Particle>& getParticleVector();
 
+    using iterator_type = std::vector<Particle>::iterator;
+
+    /**
+     * @brief Creates a container, using the given vector to initialize particles.
+     * @param particles stores the particles.
+     */
+    explicit ParticleContainer(std::vector<Particle> particles);
+
+    ParticleContainer() = default;
+
+    /**
+     * @brief Creates an iterator at the starting position.
+     * @return the iterator.
+     */
+    iterator_type begin();
+
+    /**
+     * @brief Creates an iterator pointing behind the last particle.
+     * @return the iterator.
+     */
+    iterator_type end();
+
+    /**
+     * @brief Adds the given particle to the collection.
+     * @param particle is the particle to be added.
+     */
+    void addParticle(Particle particle);
+
+    /**
+     * @brief Adds the given particles to the collection.
+     * @param toAdd contains the particles to be added.
+     */
+    void addParticles(std::vector<Particle> toAdd);
+
+    /**
+     * @brief Creates an iterator for pairs of particles at the starting position
+     * @return the iterator.
+     */
+    PairIterator beginPair();
+
+    /**
+     * @brief Creates an iterator for pairs pointing behind the last pair of particles.
+     * @return the iterator.
+     */
+    PairIterator endPair();
+
+    /**
+     * @brief Returns a vector of all stored particles.
+     * @return the vector.
+     */
+    std::vector<Particle> &getParticleVector();
+
+    /**
+     * @brief Implements an iterator that simulated a nested loop (two for loops), but skips the case where the particle in the outer loop is the same as the particle in the inner loop.
+     */
     class PairIterator {
     private:
+        /**
+         * @brief Specifies the position in the outer loop.
+         */
         std::vector<Particle>::size_type outerIndex;
+        /**
+         * @brief Specifies the position in the inner loop.
+         */
         std::vector<Particle>::size_type innerIndex;
-        std::vector<Particle>& p;
+        /**
+         * @brief Stores a reference to the particles.
+         */
+        std::vector<Particle> &p;
+
+        /**
+         * @brief Returns true if the iterator can be safely dereferenced.
+         * @return the resulting value.
+         */
         bool inRange() const;
+
     public:
-        PairIterator(std::vector<Particle>::size_type outer, std::vector<Particle>::size_type inner, std::vector<Particle>& vec);
-        std::pair<Particle&, Particle&> operator*() const;
-        PairIterator& operator++();
-        friend bool operator==(const PairIterator& a, const PairIterator& b);
-        friend bool operator!=(const PairIterator& a, const PairIterator& b);
+        /**
+         * @brief Creates an iterator at the given position.
+         * @param outer is the position in the outer loop.
+         * @param inner is the position in the inner loop.
+         * @param vec contains the particles that are iterated over.
+         */
+        PairIterator(std::vector<Particle>::size_type outer, std::vector<Particle>::size_type inner,
+                     std::vector<Particle> &vec);
+
+        /**
+         * @brief Returns a pair of particles at the position of the iterator.
+         * @return The first particle is the one in the outer loop, the second one in the inner loop.
+         */
+        std::pair<Particle &, Particle &> operator*() const;
+
+        /**
+         * @brief Increments the iterator.
+         * @return The incremented iterator.
+         */
+        PairIterator &operator++();
+
+        /**
+         * @brief Checks if the two iterators are equal.
+         * @param a Left iterator.
+         * @param b Right iterator.
+         * @return Returns true if they are equal and false otherwise.
+         */
+        friend bool operator==(const PairIterator &a, const PairIterator &b);
+
+        /**
+         * @brief Checks if the two iterators are not equal.
+         * @param a Left iterator.
+         * @param b Right iterator.
+         * @return Returns true if they are not equal and false otherwise
+         */
+        friend bool operator!=(const PairIterator &a, const PairIterator &b);
     };
 };
 
