9fa9c84df1203bb5372047a7f78573eba59bcd4a
Rodrigo Felix <rodff@localhost.localdomain>
2023-11-11 15:19:21 +0100

    Added GooogleTest as the new test framework and created some basic functionality tests for ParticleContainer

---
+++ test/ParticleContainerTest.h
@@ -0,0 +1,17 @@
+#ifndef PARTICLE_CONTAINER_TEST_H
+#define PARTICLE_CONTAINER_TEST_H
+
+#include <gtest/gtest.h>
+#include "../src/ParticleContainer.h"
+
+#include <vector>
+
+class ParticleContainerTest : public testing::Test {
+protected:
+    ParticleContainer container;
+    std::vector<Particle> particles;
+
+    void SetUp() override;
+};
+
+#endif // PARTICLE_CONTAINER_TEST_H
