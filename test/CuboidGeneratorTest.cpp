ffd48d96b6180e9e3e47a6484ffd8ecd064a1dfe
ge95dop <ge95dop@mytum.de>
2023-11-15 23:06:21 +0100

    cuboid generator test

---
+++ test/CuboidGeneratorTest.cpp
@@ -0,0 +1,25 @@
+//
+// Created by klara on 15.11.23.
+//
+
+#include "CuboidGeneratorTest.h"
+
+TEST_F(CuboidGeneratorTest, Generating){
+    ParticleContainer container = generator.generateParticles();
+    std::array<double, 3> velocity{};
+    double mass = 1;
+    std::vector<Particle> result = container.getParticleVector();
+    ASSERT_EQ(result.size(), 12);
+    ASSERT_EQ(result[0], Particle(std::array<double, 3>{0, 0, 0}, velocity, mass));
+    ASSERT_EQ(result[1], Particle(std::array<double, 3>{0, 0, 1}, velocity, mass));
+    ASSERT_EQ(result[2], Particle(std::array<double, 3>{0, 0, 2}, velocity, mass));
+    ASSERT_EQ(result[3], Particle(std::array<double, 3>{0, 1, 0}, velocity, mass));
+    ASSERT_EQ(result[4], Particle(std::array<double, 3>{0, 1, 1}, velocity, mass));
+    ASSERT_EQ(result[5], Particle(std::array<double, 3>{0, 1, 2}, velocity, mass));
+    ASSERT_EQ(result[6], Particle(std::array<double, 3>{1, 0, 0}, velocity, mass));
+    ASSERT_EQ(result[7], Particle(std::array<double, 3>{1, 0, 1}, velocity, mass));
+    ASSERT_EQ(result[8], Particle(std::array<double, 3>{1, 0, 2}, velocity, mass));
+    ASSERT_EQ(result[9], Particle(std::array<double, 3>{1, 1, 0}, velocity, mass));
+    ASSERT_EQ(result[10], Particle(std::array<double, 3>{1, 1, 1}, velocity, mass));
+    ASSERT_EQ(result[11], Particle(std::array<double, 3>{1, 1, 2}, velocity, mass));
+}
---
+++ test/CuboidGeneratorTest.h
@@ -0,0 +1,17 @@
+//
+// Created by klara on 15.11.23.
+//
+
+#ifndef PSEMOLDYN_GROUPD_CUBOIDGENERATORTEST_H
+#define PSEMOLDYN_GROUPD_CUBOIDGENERATORTEST_H
+
+#include <gtest/gtest.h>
+#include "../src/CuboidGenerator.h"
+
+class CuboidGeneratorTest : public testing::Test {
+protected:
+    CuboidGenerator generator{std::array<double, 3>{0, 0, 0}, 2, 2, 3, 1, 1, std::array<double, 3>{0}};
+};
+
+
+#endif //PSEMOLDYN_GROUPD_CUBOIDGENERATORTEST_H
