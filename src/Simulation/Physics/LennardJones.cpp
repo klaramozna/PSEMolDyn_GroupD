d02915ceb3c394794d2afaa94065a442e01768c6
RayenManai <rayenmannai77@gmail.com>
2023-11-30 23:42:33 +0100

    deleted file logger(not needed) + removed pow() from force calculation

--- src/Simulation/Physics/LennardJones.cpp
+++ src/Simulation/Physics/LennardJones.cpp
@@ -1,7 +1,6 @@
 #include "LennardJones.h"
 #include "../../utils/VectorDouble3.h"
 
-#include <complex>
 
 VectorDouble3 LennardJones::CalculateForces(const Particle &p1, const Particle &p2) {
         VectorDouble3 result{};
@@ -12,6 +11,7 @@ VectorDouble3 LennardJones::CalculateForces(const Particle &p1, const Particle &
         double sigma_term_pow2 = sigma_term * sigma_term;
         double sigma_term_pow_6 = sigma_term_pow2 * sigma_term_pow2 * sigma_term_pow2;
         double sigma_term_pow_12 = sigma_term_pow_6 * sigma_term_pow_6;
-        result = (-24 * eps / pow(distanceL2Norm,2)) * (sigma_term_pow_6 - 2 * sigma_term_pow_12) * distance;
+        double distanceL2Norm_pow2 = distanceL2Norm * distanceL2Norm;
+        result = (-24 * eps / distanceL2Norm_pow2) * (sigma_term_pow_6 - 2 * sigma_term_pow_12) * distance;
         return result;
 };
