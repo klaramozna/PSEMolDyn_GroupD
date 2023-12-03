73867c8e19f86423f1482809ed8c6ac9f7503ebc
ge95dop <ge95dop@mytum.de>
2023-12-03 23:34:32 +0100

    fixed test

--- test/LinkedCellContainerTest.cpp
+++ test/LinkedCellContainerTest.cpp
@@ -7,6 +7,11 @@
 
 
 TEST_F(LinkedCellContainerTest, Initialization){
+    LennardJones force{1, 1};
+    OutflowBoundary boundary{std::array<double, 3>{0, 0, 0}, std::array<double, 3>{4, 4, 3}, force};
+    LinkedCellContainer container{boundary, 2};
+    std::vector<Particle> particles{p1, p2, p3, p4};
+    container.addParticles(particles);
     std::vector<Cell> grid = container.getCells();
     ASSERT_TRUE(grid[9].contains(p1));
     ASSERT_TRUE(grid[22].contains(p3));
@@ -15,7 +20,4 @@ TEST_F(LinkedCellContainerTest, Initialization){
 }
 
 
-void LinkedCellContainerTest::SetUp() {
-    std::vector<Particle> particles{p1, p2, p3, p4};
-    container.addParticles(particles);
-}
+
