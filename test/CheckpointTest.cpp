4c11dbeb1fb00dc4ebd7a98149075ab0192489df
RayenManai <rayenmannai77@gmail.com>
2023-12-11 11:20:25 +0100

    added tests for checkpoint feature

---
+++ test/CheckpointTest.cpp
@@ -0,0 +1,34 @@
+#include "CheckpointTest.h"
+
+void CheckpointTest::SetUp() {
+    filename = std::string(TEST_RESOURCES_DIR);
+    std::vector<Particle> particles{p1, p2, p3, p4};
+    container.addParticles(particles);
+}
+
+/**
+ * @brief state will be save to a checkpoint file and then read again
+ * @result read_container must contain the same particles as saved
+ **/
+TEST_F(CheckpointTest, TestSimpleCheckpoint) {
+
+    filename.append("Checkpoints/checkpoint1.xml");
+    //cWriter.writeCheckpoint(container, filename);
+
+    
+    LinkedCellContainer read_container{Boundary(4, 4, 4, fc, 1), 1};
+    cReader.readFile(read_container, filename, simParameters);
+    
+    ASSERT_EQ(read_container.getSize(), container.getSize()) << "Containers didn't match in size";
+    auto it1 = container.getParticleVector().begin();
+    auto it2 = read_container.getParticleVector().begin();
+
+    for (; it1 != container.getParticleVector().end() && it2!= read_container.getParticleVector().end(); ++it1, ++it2) {
+        Particle p1 = *it1;
+        Particle p2 = *it2;
+
+        ASSERT_TRUE(p1 == p2) << "Expected " << p1.toString() << "\nReceived " << p2.toString();
+    }
+};
+
+
---
+++ test/CheckpointTest.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <string>
+
+#include "../src/IO/input/CheckpointReader.h"
+#include "../src/IO/output/outputWriter/CheckpointWriter.h"
+#include "../src/IO/input/SimParameters.h"
+#include "../src/Particles/LinkedCellContainer.h"
+#include "../src/Particles/Boundary.h"
+
+// Define a mock ForceCalculation class for testing
+class MockForceCalculation : public ForceCalculation {
+public:
+    // Override CalculateForces to return a known force vector for testing
+    VectorDouble3 CalculateForces(const Particle& p1, const Particle& p2) override {
+        // You can customize this for your specific testing needs
+        return p1.getXVector() - p2.getXVector();
+    }
+};
+
+
+class CheckpointTest : public testing::Test {
+protected:
+    CheckpointWriter cWriter;
+    CheckpointReader cReader;
+    std::string filename;
+    MockForceCalculation fc{};
+    LinkedCellContainer container{Boundary(4, 4, 4, fc, 1), 1};
+    Particle p1{std::array<double, 3>{0, 0, 0}, std::array<double, 3>{1, 1, 1}, 1};
+    Particle p2{std::array<double, 3>{1, 0.5, 2}, std::array<double, 3>{1, 1, 1}, 1};
+    Particle p3{std::array<double, 3>{3, 2, 0}, std::array<double, 3>{1, 1, 1}, 1};
+    Particle p4{std::array<double, 3>{1, 0.3, 2}, std::array<double, 3>{1, 1, 1}, 1};
+    SimParameters simParameters;
+    void SetUp() override;
+
+};
