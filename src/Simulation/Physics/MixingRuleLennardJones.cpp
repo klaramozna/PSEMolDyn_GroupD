d836010a5846129258edb37b50cd6478422885f7
ge95dop <ge95dop@mytum.de>
2023-12-12 19:25:30 +0100

    Added new class for force calculation with mixing rules.

---
+++ src/Simulation/Physics/MixingRuleLennardJones.cpp
@@ -0,0 +1,9 @@
+//
+// Created by klara on 12.12.23.
+//
+
+#include "MixingRuleLennardJones.h"
+
+VectorDouble3 MixingRuleLennardJones::CalculateForces(const Particle &p1, const Particle &p2) {
+
+}
---
+++ src/Simulation/Physics/MixingRuleLennardJones.h
@@ -0,0 +1,20 @@
+
+
+#ifndef PSEMOLDYN_GROUPD_MIXINGRULELENNARDJONES_H
+#define PSEMOLDYN_GROUPD_MIXINGRULELENNARDJONES_H
+
+#include "ForceCalculation.h"
+
+class MixingRuleLennardJones : public ForceCalculation{
+
+    /**
+     * @brief Calculates the Lennard Jones forces between the two particles. The parameters sigma and epsilon are determined based on the type of the particles using mixing rules.
+     * @param p1 Particle which should be considered in the force calculation.
+     * @param p2 Particle which should be considered in the force calculation.
+     * @return The resulting force between the particles.
+     */
+    VectorDouble3 CalculateForces(const Particle& p1, const Particle& p2) override;
+};
+
+
+#endif //PSEMOLDYN_GROUPD_MIXINGRULELENNARDJONES_H
