4fd9b1b4893276eb781f4649be0fb1f1ba44010c
RayenManai <rayenmannai77@gmail.com>
2023-12-16 09:43:03 +0100

    merged with nain

--- src/Simulation/Physics/GravityForce.cpp
+++ src/Simulation/Physics/GravityForce.cpp
@@ -5,7 +5,7 @@ GravityForce::GravityForce(double gravity_factor_val) {
     gravity_factor = gravity_factor_val;
 }
 
-VectorDouble3 GravityForce::CalculateForces(const Particle &p) {
+VectorDouble3 GravityForce::CalculateForce(const Particle &p) {
     //gravitational force G (along the y-axis) 
     double gravity = - p.getM()*gravity_factor;
     VectorDouble3 result (std::array<double,3> {0.0, gravity , 0.0});
--- src/Simulation/Physics/GravityForce.h
+++ src/Simulation/Physics/GravityForce.h
@@ -5,7 +5,7 @@
 class GravityForce: public OneParticleForceCalculation {
 public:
     GravityForce(double gravity_factor);
-    VectorDouble3 CalculateForces(const Particle& p) override;
+    VectorDouble3 CalculateForce(const Particle& p) override;
     ~GravityForce() = default;
 
 private:
