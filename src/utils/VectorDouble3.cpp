07be9d3f9d3d0a2c0744d71ade017a772c447dec
ge95dop <ge95dop@mytum.de>
2023-12-20 19:37:43 +0100

    Moved SIMD operations to normal operators.

--- src/utils/VectorDouble3.cpp
+++ src/utils/VectorDouble3.cpp
@@ -5,21 +5,29 @@
 #include "VectorDouble3.h"
 #include <cmath>
 #include <iostream>
+#include <immintrin.h>
 
 using lui = long unsigned int;
 
 VectorDouble3 VectorDouble3::operator+=(const VectorDouble3 &other) {
-    for (lui i = 0; i < values.size(); i++) {
+    /*for (lui i = 0; i < values.size(); i++) {
         values[i] += other.values[i];
     }
-    return *this;
+    return *this;*/
+
+    VectorDouble3 add = (*this) + other;
+    values = add.values;
+    return add;
 }
 
 VectorDouble3 VectorDouble3::operator-=(const VectorDouble3 &other) {
-    for (lui i = 0; i < values.size(); i++) {
+    /*for (lui i = 0; i < values.size(); i++) {
         values[i] -= other.values[i];
     }
-    return *this;
+    return *this;*/
+    VectorDouble3 subtract = (*this) - other;
+    values = subtract.values;
+    return subtract;
 }
 
 double VectorDouble3::getL2Norm() const {
@@ -37,27 +45,64 @@ void VectorDouble3::print() const {
 }
 
 VectorDouble3 operator+(const VectorDouble3 &vec1, const VectorDouble3 &vec2) {
-    VectorDouble3 result{};
+    /*VectorDouble3 result{};
     for (lui i = 0; i < result.values.size(); i++) {
         result.values[i] = vec1.values[i] + vec2.values[i];
     }
-    return result;
+    return result;*/
+
+    // Load registers
+    __m256d simd_a = _mm256_loadu_pd(vec1.values.data());
+    __m256d simd_b = _mm256_loadu_pd(vec2.values.data());
+
+    // Calculate result
+    __m256d simd_result = _mm256_add_pd(simd_a, simd_b);
+
+    // Return results
+    std::array<double, 4> result{};
+    _mm256_storeu_pd(result.data(), simd_result);
+    return VectorDouble3(std::array<double, 3>{result[0], result[1], result[2]});
 }
 
 VectorDouble3 operator-(const VectorDouble3 &vec1, const VectorDouble3 &vec2) {
-    VectorDouble3 result{};
+    /*VectorDouble3 result{};
     for (lui i = 0; i < result.values.size(); i++) {
         result.values[i] = vec1.values[i] - vec2.values[i];
     }
-    return result;
+    return result;*/
+
+    // Load registers
+    __m256d simd_a = _mm256_loadu_pd(vec1.values.data());
+    __m256d simd_b = _mm256_loadu_pd(vec2.values.data());
+
+    // Calculate result
+    __m256d simd_result = _mm256_sub_pd(simd_a, simd_b);
+
+    // Return results
+    std::array<double, 4> result{};
+    _mm256_storeu_pd(result.data(), simd_result);
+    return VectorDouble3(std::array<double, 3>{result[0], result[1], result[2]});
 }
 
 VectorDouble3 operator*(double scalar, const VectorDouble3 &vec) {
-    VectorDouble3 result{};
+    /*VectorDouble3 result{};
     for (lui i = 0; i < result.values.size(); i++) {
         result.values[i] = scalar * vec.values[i];
     }
-    return result;
+    return result;*/
+
+    // Load registers
+    std::array<double, 4> scalarArray{scalar, scalar, scalar, scalar};
+    __m256d simd_a = _mm256_loadu_pd(vec.values.data());
+    __m256d simd_b = _mm256_loadu_pd(scalarArray.data());
+
+    // Calculate result
+    __m256d simd_result = _mm256_mul_pd(simd_a, simd_b);
+
+    // Return results
+    std::array<double, 4> result{};
+    _mm256_storeu_pd(result.data(), simd_result);
+    return VectorDouble3(std::array<double, 3>{result[0], result[1], result[2]});
 }
 
 VectorDouble3::VectorDouble3(const std::array<double, 3> &array): values{} {
